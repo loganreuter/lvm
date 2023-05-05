1d76fe61e4d1de81a06fe33f49a049853ad05a43
lreuter2020 <lreuter2020@gmail.com>
2023-05-04 21:34:58 -0700

    created conditional operations

---
+++ lvm/conditional.h
@@ -0,0 +1,11 @@
+#ifndef CONDITIONAL_H
+#define CONDITIONAL_H
+
+    #include "stdint.h"
+    #include "machine.h"
+
+    static inline void CMP(uint32_t i) {}
+    static inline void JMP(uint32_t i) {}
+    static inline void JCOND(uint32_t i) {}
+    
+#endif
