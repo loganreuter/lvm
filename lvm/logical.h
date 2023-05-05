650babe063115172eab5eb9ce83214d6225e584c
lreuter2020 <lreuter2020@gmail.com>
2023-05-04 21:32:50 -0700

    added logic operation functions

--- lvm/logical.h
+++ lvm/logical.h
@@ -1,4 +1,13 @@
 #ifndef LOGICAL_H
 #define LOGICAL_H
 
+#include "stdint.h"
+#include "machine.h"
+
+static inline void AND(uint32_t i){}
+static inline void OR(uint32_t i) {}
+static inline void XOR(uint32_t i) {}
+static inline void TEST(uint32_t i) {}
+static inline void NOT(uint32_t i) {}
+
 #endif
